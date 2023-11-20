cba01353980c982519374f21e17a022d27831e23
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-11-20 12:33:28 +0530

    Updated Position entity and PositionDTO classes

--- Back-End/src/main/java/lk/mindup/entity/Positions.java
+++ Back-End/src/main/java/lk/mindup/entity/Positions.java
@@ -17,6 +17,11 @@ import javax.persistence.ManyToOne;
 public class Positions {
     @Id
     private String position_id;
+    private String company_name;
+    private String position;
+    private String start_date;
+    private String end_date;
+    private String description;
 
     @ManyToOne
     private User user;
