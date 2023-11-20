f7f4849ffbed6b7b010237b7c966dae23483db7f
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-11-20 20:43:32 +0530

    Created saveUser handle method of loginController

--- Back-End/src/main/java/lk/mindup/controller/LoginController.java
+++ Back-End/src/main/java/lk/mindup/controller/LoginController.java
@@ -1,10 +1,15 @@
 package lk.mindup.controller;
 
+import lk.mindup.dto.UserDTO;
+import lk.mindup.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/login")
 public class LoginController {
-
+    @PostMapping
+    public ResponseUtil saveUser(@RequestBody UserDTO userDTO){
+        return new ResponseUtil("Ok","Successfully Added...!",userDTO.getLogin().getEmail());
+    }
 }
