550438f52d236bef511ed8bb1914bfcdc3cd876f
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-11-22 12:26:47 +0530

    Created getLastFollowerId and generateNextFollowerId functions

--- Front-End/assets/js/controller/userController.js
+++ Front-End/assets/js/controller/userController.js
@@ -1,3 +1,18 @@
+
+
+function getLastFollowerId() {
+    $.ajax({
+        url: base_url + "/user/last/follower/id",
+        method: "get",
+        success: function (resp) {
+            generateNextFollowerId(resp.data);
+        },
+        error: function (resp) {
+
+        }
+    })
+}
+
 function getLastUserId() {
     $.ajax({
         url: base_url + "/user/last/user/id",
@@ -11,6 +26,13 @@ function getLastUserId() {
     })
 }
 
+function generateNextFollowerId(currentId) {
+    if (currentId === null) {
+        return "FWER-1";
+    } else {
+        return "FWER-" + (Number(currentId.slice(5)) + 1);
+    }
+}
 function generateNextUserId(currentId) {
     if (currentId === null) {
         return "USER-1";
